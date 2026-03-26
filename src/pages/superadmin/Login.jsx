3276dfd6d422b1919459ace185a605bbf4a5a492
kirti123 <kirtiubale22@gmail.com>
2026-03-26 20:59:27 +0530

    fixed api

--- src/pages/superadmin/Login.jsx
+++ src/pages/superadmin/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     }
 
     try {
-      const res = await fetch("skillhub-backend-production-e6ee.up.railway.app/login", {
+      const res = await fetch("skillhub-backend-production-e6ee.up.railway.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
